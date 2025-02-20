c0528da1388e7ba9b3839182900ca2922110ac63
Xojiakbar Isroilov <uer.oooouuuuooo@gmail.com>
2025-02-20 17:41:38 +0500

    subscribe card data

--- src/models/subscribeModel.js
+++ src/models/subscribeModel.js
@@ -2,7 +2,7 @@ const subscribePackages = [
   {
     id: 1,
     name: "Tekin",
-    price: 0,
+    price: "$0.00",
     features: [
       { id: 1, text: "Oddiy vizitka sahifasi" },
       { id: 2, text: "Minimal SEO optimizatsiya" },
@@ -14,20 +14,20 @@ const subscribePackages = [
   {
     id: 2,
     name: "Yakka tadbirkor",
-    price: 3,
+    price: "$3",
     features: [
       { id: 1, text: "Asosiy SEO optimizatsiya" },
       { id: 2, text: "Mobil moslashuvchan dizayn" },
       { id: 3, text: "Shaxsiy domen ulash imkoniyati" },
-      { id: 4, text: "Telegram orqali qo‘llab-quvvatlash" },
       { id: 5, text: "5 ta xizmat yoki mahsulot qo‘shish" },
+      { id: 4, text: "Telegram orqali qo‘llab-quvvatlash" },
       { id: 6, text: "Google va Yandex indeksatsiyasiga tayyor sayt" },
     ],
   },
   {
     id: 3,
     name: "Kichik biznes",
-    price: 5,
+    price: "$5",
     features: [
       { id: 1, text: "To‘liq SEO optimizatsiya" },
       { id: 2, text: "Blog va yangiliklar bo‘limi" },
@@ -43,7 +43,7 @@ const subscribePackages = [
   {
     id: 4,
     name: "O'rta biznes",
-    price: 12,
+    price: "$12",
     features: [
       { id: 1, text: "Premium dizayn va brending" },
       { id: 2, text: "CRM tizimi bilan integratsiya" },
@@ -60,12 +60,12 @@ const subscribePackages = [
   {
     id: 5,
     name: "Yirik biznes",
-    price: 24,
+    price: "$24",
     features: [
       { id: 1, text: "Kiberxavfsizlik kafolati" },
       { id: 2, text: "Jamoa uchun ko‘p foydalanuvchi tizimi" },
-      { id: 3, text: "Cheksiz mahsulot va xizmatlar qo‘shish" },
-      { id: 4, text: "API orqali boshqa tizimlar bilan ulash" },
+      { id: 3, text: "API orqali boshqa tizimlar bilan ulash" },
+      { id: 4, text: "Cheksiz mahsulot va xizmatlar qo‘shish" },
       { id: 5, text: "To‘liq avtomatlashtirilgan savdo tizimi" },
       { id: 6, text: "24/7 shaxsiy menejer qo‘llab-quvvatlovi" },
       { id: 7, text: "Raqamli marketing strategiyasini ishlab chiqish" },
@@ -74,7 +74,7 @@ const subscribePackages = [
   {
     id: 6,
     name: "Korxona",
-    price: 48,
+    price: "$48",
     features: [
       { id: 1, text: "Korporativ e-mail va hosting" },
       { id: 2, text: "Kompaniya ichki portalini yaratish" },
