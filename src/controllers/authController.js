43cbb858f022ed91f15aae527bad01517ea72a03
Xojiakbar Isroilov <uer.oooouuuuooo@gmail.com>
2025-02-21 21:35:40 +0500

    auth changed

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,16 +1,40 @@
+const bcrypt = require("bcryptjs");
 const users = require("../models/authModel");
 
-const login = (req, res) => {
+const signin = async (req, res) => {
   const { username, password } = req.body;
-  const user = users.find(
-    (u) => u.username === username && u.password === password
-  );
+  const user = users.find((u) => u.username === username);
 
-  if (user) {
-    res.json({ message: "Login successful", user });
-  } else {
-    res.status(401).json({ message: "Invalid credentials" });
+  if (!user) {
+    return res.status(401).json({ message: "Invalid credentials" });
   }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    return res.status(401).json({ message: "Invalid credentials" });
+  }
+
+  res.json({ message: "Signin successful", user });
 };
 
-module.exports = { login };
+const signup = async (req, res) => {
+  const { username, password } = req.body;
+
+  const existingUser = users.find((u) => u.username === username);
+  if (existingUser) {
+    return res.status(400).json({ message: "Username already exists" });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = {
+    id: users.length + 1,
+    username,
+    password: hashedPassword,
+  };
+  users.push(newUser);
+
+  res.status(201).json({ message: "User registered successfully", newUser });
+};
+
+module.exports = { signin, signup };
--- src/models/authModel.js
+++ src/models/authModel.js
@@ -1,3 +1,7 @@
-const users = [{ id: 1, username: "admin", password: "123456" }];
+const bcrypt = require("bcryptjs");
+
+const hashedPassword = bcrypt.hashSync("123456", 10);
+
+const users = [{ id: 1, username: "admin", password: hashedPassword }];
 
 module.exports = users;
