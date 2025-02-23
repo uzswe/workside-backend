e05ab33f16b93eac7cd75add350589b469c4f043
Xojiakbar Isroilov <uer.oooouuuuooo@gmail.com>
2025-02-23 20:44:37 +0500

    social data changed

--- src/models/socialData.js
+++ src/models/socialData.js
@@ -1,11 +1,10 @@
 const social_data = [
-  { name: "Instagram", value: 150000, icon: "RiInstagramLine" },
+  { name: "TikTok", value: 120000, icon: "RiTiktokFill" },
+  { name: "YouTube", value: 230000, icon: "RiYoutubeFill" },
   { name: "Telegram", value: 64000, icon: "RiTelegramFill" },
   { name: "Facebook", value: 42000, icon: "RiFacebookFill" },
-  { name: "YouTube", value: 230000, icon: "RiYoutubeFill" },
-  { name: "Twitter", value: 89000, icon: "RiTwitterFill" },
   { name: "LinkedIn", value: 35000, icon: "RiLinkedinFill" },
-  { name: "TikTok", value: 120000, icon: "RiTiktokFill" },
+  { name: "Instagram", value: 150000, icon: "RiInstagramLine" },
 ];
 
 module.exports = social_data;
