7b78b5f8b44c79e88d87e40df79aa8087ab232c6
Xojiakbar Isroilov <uer.oooouuuuooo@gmail.com>
2025-02-23 21:39:31 +0500

    social data

--- src/models/socialData.js
+++ src/models/socialData.js
@@ -1,9 +1,7 @@
 const social_data = [
-  { name: "TikTok", value: 120000, icon: "RiTiktokFill" },
   { name: "YouTube", value: 230000, icon: "RiYoutubeFill" },
   { name: "Telegram", value: 64000, icon: "RiTelegramFill" },
   { name: "Facebook", value: 42000, icon: "RiFacebookFill" },
-  { name: "LinkedIn", value: 35000, icon: "RiLinkedinFill" },
   { name: "Instagram", value: 150000, icon: "RiInstagramLine" },
 ];
 
