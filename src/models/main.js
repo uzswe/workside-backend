3486dac12dbe0029ab0e27a51ce90cb15d2bf64c
Xojiakbar Isroilov <uer.oooouuuuooo@gmail.com>
2025-02-23 18:29:53 +0500

    main data

---
+++ src/models/main.js
@@ -0,0 +1,91 @@
+const main_top_card_data = [
+  {
+    id: 1,
+    name: "Instagram",
+    icon: "RiInstagramLine",
+    url: "https://instagram.com",
+    value: "150k",
+  },
+  {
+    id: 2,
+    name: "Telegram",
+    icon: "RiTelegramFill",
+    url: "https://t.me",
+    value: "64k",
+  },
+  {
+    id: 3,
+    name: "Facebook",
+    icon: "RiFacebookFill",
+    url: "https://facebook.com",
+    value: "42k",
+  },
+  {
+    id: 4,
+    name: "YouTube",
+    icon: "RiYoutubeFill",
+    url: "https://youtube.com",
+    value: "230k",
+  },
+  {
+    id: 5,
+    name: "Twitter",
+    icon: "RiTwitterFill",
+    url: "https://twitter.com",
+    value: "89k",
+  },
+];
+
+module.exports = main_top_card_data;
+
+const main_middle_card_data = [
+  {
+    id: 1,
+    name: "Instagram",
+    icon: "RiInstagramLine",
+    url: "https://instagram.com",
+    value: "150k",
+  },
+  {
+    id: 2,
+    name: "Telegram",
+    icon: "RiTelegramFill",
+    url: "https://t.me",
+    value: "64k",
+  },
+  {
+    id: 3,
+    name: "Facebook",
+    icon: "RiFacebookFill",
+    url: "https://facebook.com",
+    value: "42k",
+  },
+  {
+    id: 4,
+    name: "LinkedIn",
+    icon: "RiLinkedinFill",
+    url: "https://linkedin.com",
+    value: "35k",
+  },
+  {
+    id: 5,
+    name: "TikTok",
+    icon: "RiTiktokFill",
+    url: "https://tiktok.com",
+    value: "120k",
+  },
+];
+
+module.exports = main_middle_card_data;
+
+const bar_chart_data = [
+  { name: "Instagram", value: 150000 },
+  { name: "Telegram", value: 64000 },
+  { name: "Facebook", value: 42000 },
+  { name: "YouTube", value: 230000 },
+  { name: "Twitter", value: 89000 },
+  { name: "LinkedIn", value: 35000 },
+  { name: "TikTok", value: 120000 },
+];
+
+module.exports = bar_chart_data;
--- src/models/profileModel.js
+++
@@ -1,7 +0,0 @@
-const profiles = [
-  { id: 1, name: "Instagram", icon: "RiInstagramLine", url: "" },
-  { id: 2, name: "Telegram", icon: "RiTelegramFill", url: "" },
-  { id: 3, name: "Github", icon: "RiGithubFill", url: "" },
-];
-
-module.exports = profiles;
